f296b3b0ac5c90afb53c3023966ad041a29a3ad3
GStagg-1 <gavinstagg22@gmail.com>
2025-04-08 13:44:49 -0700

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,25 @@
+package Pack1;
+
+//Psuedocode
+//Program Start
+//Assign variable a=20, b=1.5
+//Create new object, Calculate which will print the result of a * b
+// Run Calculate
+// Value = 30.0
+public class Calculator {
+static int a = 20;
+static double b = 1.5;  
+
+	public static void main(String[] args) {
+		Calculator myobject = new Calculator();
+		myobject.Calculate(); }
+		
+	public void Calculate() {
+		System.out.println(a*b);
+	
+		
+	}
+	
+	}
+
+
