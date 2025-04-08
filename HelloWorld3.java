e0afd2ada11cef6d662799bb03672821ccd8d824
GStagg-1 <gavinstagg22@gmail.com>
2025-04-08 11:30:51 -0700

    Add files via upload

---
+++ HelloWorld3.java
@@ -0,0 +1,12 @@
+package hellowrld;
+
+public class HelloWorld {
+			static string("Hello") = Hello World
+					
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+System.out.println(Hello);
+	}
+
+}
